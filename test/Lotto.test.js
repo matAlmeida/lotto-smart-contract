f44305b45051c74a04c6211188f92b86f39cce6c
Matheus Almeida <mat.almeida@live.com>
2018-11-30 16:23:02 -0300

    test: lotto: raffle

--- test/Lotto.test.js
+++ test/Lotto.test.js
@@ -14,12 +14,10 @@ describe('Lotto', () => {
     arguments: defaultArguments
   };
 
+  const sendEthers = (ethers) => web3.utils.toWei(ethers, 'ether');
+
   let accounts;
   let lotto;
-
-  const getMessage = async () => await lotto.methods.message().call();
-  const sendMessage = async (message) =>
-    await lotto.methods.setMessage(message).send({ from: accounts[0] });
 
   beforeEach('Recreate Contract', async () => {
     accounts = await web3.eth.getAccounts();
@@ -28,33 +26,54 @@ describe('Lotto', () => {
       .send({ from: accounts[0], gas: 1000000 });
   });
 
-  it('Deploy a contract', async () => {
-    expect(lotto.options.address).to.be.ok;
-  });
-
-  it('Has a creator', async () => {
-    const creator = accounts[0];
-    const retrivedCreator = await lotto.methods.creator().call();
-
-    expect(retrivedCreator).to.equal(creator);
-  });
-
-  it('Get Player', () => {
-    const players = await lotto.methods.getPlayers().call();
-    expect(players).to.equal([]);
-  });
-
-  it('Is playable!', async () => {
-    const player = accounts[0];
-    const playersBeforePlay = await lotto.methods.getPlayers().call();
-    await lotto.methods.play().send({
-      from: player,
-      value: web3.utils.toWei('0.1', 'ether')
+  describe('Deploying', () => {
+    it('Deploy a contract', async () => {
+      expect(lotto.options.address).to.be.ok;
     });
-    const playersAfterPlay = await lotto.methods.getPlayers().call();
 
-    expect(playersAfterPlay.length).to.be.greaterThan(playersBeforePlay.length);
-    expect(playersAfterPlay.length).to.equal(1);
-    expect(playersAfterPlay[0]).to.equal(player);
+    it('Has a creator', async () => {
+      const creator = accounts[0];
+      const retrivedCreator = await lotto.methods.creator().call();
+
+      expect(retrivedCreator).to.equal(creator);
+    });
+
+    it('Get Player', async () => {
+      const players = await lotto.methods.getPlayers().call();
+      expect(players.length).to.equal(0);
+    });
+  });
+
+  describe('Playing', () => {
+    it('Is playable!', async () => {
+      const player = accounts[0];
+      const playersBeforePlay = await lotto.methods.getPlayers().call();
+      await lotto.methods.play().send({
+        from: player,
+        value: sendEthers('0.1')
+      });
+      const playersAfterPlay = await lotto.methods.getPlayers().call();
+
+      expect(playersAfterPlay.length).to.be.greaterThan(
+        playersBeforePlay.length
+      );
+      expect(playersAfterPlay.length).to.equal(1);
+      expect(playersAfterPlay[0]).to.equal(player);
+    });
+  });
+
+  describe('Raffle', () => {
+    it('Creator play once and Win!', async () => {
+      const creator = accounts[0];
+
+      await lotto.methods.play().send({
+        from: creator,
+        value: sendEthers('0.1')
+      });
+
+      const winner = await lotto.methods.raffle().call();
+
+      expect(winner).to.equal(creator);
+    });
   });
 });
