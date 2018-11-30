402c671e7bc9416239bc607ae157425e12aebe53
Matheus Almeida <mat.almeida@live.com>
2018-11-30 15:13:20 -0300

    fix: compiler

--- src/compile.js
+++ src/compile.js
@@ -6,7 +6,9 @@ module.exports = (contractPath) => {
   const inboxPath = path.resolve(__dirname, contractPath);
   const source = fs.readFileSync(inboxPath, 'utf8');
 
-  const contractName = contractPath.split('/')[-1].split('.')[0];
+  const nameReg = /.*\/([a-zA-Z]*).sol/gm.exec(inboxPath);
+
+  const contractName = nameReg[1];
 
   return solc.compile(source, 1).contracts[`:${contractName}`];
 };
